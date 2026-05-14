b174a75a196560fb2a70e204b7959a2023c5ff61
Victor dos Santos Matos <smatosvictor@gmail.com>
2026-05-13 22:33:17 -0300

    Add files via upload

---
+++ Aula 14/javascript.js
@@ -0,0 +1,27 @@
+var x = 5
+if (x > 5) {
+    console.log("x is greater than 5");
+
+} else if (x < 5) {
+    console.log("x is less than 5");
+} else {
+    console.log("x is equal to 5"); 'x is equal to 5'
+}
+for (var i = 0; i < 10; i++){
+if (i % 2=== 0) {
+    console.log(i)
+};
+}
+
+for (let i = 0; i < 10; i += 2) {
+    console.log(i)
+};
+
+for (let i = 1; 1 < 10; i += 2) {
+ console.log(i); 1, 3, 5, 7, 9   
+};
+var y=0;
+while (y < 10) {
+    console.log(y); 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    y++;
+};
